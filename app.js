90ff3cb969a7824dfe7afb38282220f61a8efc2d
Promi Mojumder <74852911+PromiMojumder38@users.noreply.github.com>
2023-07-05 09:57:11 +0600

    remove unnecessary files and backend fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const app = express();
-const port = 5000;
+const port = 4003;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
